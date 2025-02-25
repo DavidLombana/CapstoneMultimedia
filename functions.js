1165dba6ae570420c28e9afeeaf8647ca75d339d
DavidLombana <davidlombana01@gmail.com>
2025-02-25 00:31:42 +0000

    changes

--- functions.js
+++ functions.js
@@ -1,23 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const images = document.querySelectorAll(".block img");
-
-    images.forEach((img) => {
-        img.addEventListener("mouseenter", function () {
-            img.style.transform = "scale(1.2)";
-            img.style.transition = "transform 0.3s ease-in-out";
-        });
-
-        img.addEventListener("mouseleave", function () {
-            img.style.transform = "scale(1)";
-        });
-    });
-});
-document.addEventListener("DOMContentLoaded", function () {
     const blocks = document.querySelectorAll(".block");
 
     blocks.forEach((block) => {
         block.addEventListener("click", function () {
-            block.classList.toggle("flipped");
+            this.classList.toggle("flipped");
         });
     });
 });
